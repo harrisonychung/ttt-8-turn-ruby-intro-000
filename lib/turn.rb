53394793c58dc310430e8e52752b94ca0e6af35c
Harrison Chung <harrison.y.chung@gmail.com>
2020-08-31 18:12:06 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,3 +8,9 @@ def display_board(board)
 
 def valid_move?(board, index)
 def position_taken(array, ind)
+  if array[ind] == " " || array[ind] == "" || array[ind] == nil
+    return false
+  else
+    return true
+  end
+end
